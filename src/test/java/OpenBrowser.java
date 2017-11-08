dc16e6ddc81109f6b21edd2f549816c9c0eae4f9
Himani Bansal <hbansal@XEBIAINDIA.LOCAL>
2017-11-08 12:19:32 +0530

    first commit

---
+++ src/test/java/OpenBrowser.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class OpenBrowser {
+
+    @Parameters("browser")
+    @Test
+    public void paralleltesting(String browser)
+    {
+        if(browser.equals("firefox"))
+        {
+           String exepath="C:\\Users\\hbansal\\IdeaProjects\\SampleMavenProject\\src\\main\\resources\\drivers\\geckodriver.exe";
+           System.setProperty("webdriver.gecko.driver",exepath);
+            DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+            //capabilities.setCapability("marionette", true);
+            WebDriver driver = new FirefoxDriver();
+            driver.get("http://google.com");
+        }
+        else if(browser.equals("chrome"))
+        {
+            String exePath = "C:\\Users\\hbansal\\IdeaProjects\\SampleMavenProject\\src\\main\\resources\\drivers\\chromedriver.exe";
+            System.setProperty("webdriver.chrome.driver", exePath);
+            WebDriver driver = new ChromeDriver();
+            driver.get("http://google.com");
+
+        }
+
+    }
+}
+
+
